bf29f2857397a3b73af39bc6d460d39ed2e4478f
h3canlao <vtp336@gmail.com>
2025-12-29 11:58:47 +0700

    Fix: Add HttpClientFactory to DI container

--- E-Education.API/Program.cs
+++ E-Education.API/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
 
 // Add JWT Authentication
 var jwtSettings = builder.Configuration.GetSection("JwtSettings");
